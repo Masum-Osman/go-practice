5432cb3a15992d0ae4c41c7cd123c3555caa4cc9
Masum_Osman <ogkmasum@gmail.com>
2023-01-23 12:58:00 +0600

    function: Anonymous Function

--- functions/func.go
+++ functions/func.go
@@ -1,5 +1,14 @@
 package functions
 
+import "fmt"
+
+// anonymous function:
+var (
+	area = func(l int, b int) int {
+		return l * b
+	}
+)
+
 func Rectangle(l int, b int) (area int, parameter int) {
 	parameter = 2 * (l + b) // see, no `:` in this line. as the var in previously descrived on return statement
 	area = l * b
@@ -10,4 +19,15 @@ func Rectangle(l int, b int) (area int, parameter int) {
 func PassingAddress(a *int, t *string) {
 	*a = *a + 5 // `*` to point the address
 	*t = *t + " Doe"
+}
+
+func AnonymousFunc() {
+	fmt.Println(area(50, 50))
+
+	fmt.Printf(
+		"81 (°F) = %.2f (°C)\n",
+		func(f float64) float64 {
+			return (f - 32.0) * (5.0 / 9.0)
+		}(81),
+	)
 }
--- main.go
+++ main.go
@@ -18,4 +18,5 @@ func main() {
 
 	fmt.Println("After: ", name, age)
 
+	f.AnonymousFunc()
 }
