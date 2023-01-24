ca5daa83d67910cca1983cbf93e10f33f2f5ca61
Masum_Osman <ogkmasum@gmail.com>
2023-01-24 11:36:30 +0600

    function: Variadic Function

--- functions/func.go
+++ functions/func.go
@@ -1,6 +1,9 @@
 package functions
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 // anonymous function:
 var (
@@ -21,6 +24,10 @@ func PassingAddress(a *int, t *string) {
 	*t = *t + " Doe"
 }
 
+/*
+	Anonymous Functions
+*/
+
 func AnonymousFunc() {
 	fmt.Println(area(50, 50))
 
@@ -31,6 +38,10 @@ func AnonymousFunc() {
 		}(81),
 	)
 }
+
+/*
+	Closure Functions
+*/
 
 // Note: Closures are a special case of anonymous functions.
 // Closures are anonymous functions which access the variables defined outside the body of the function.
@@ -51,5 +62,77 @@ func ClosuresLoop() {
 			return i * 39.370
 		}()
 		fmt.Printf("%.2f Meter = %.2f Inch\n", i, rad)
+	}
+}
+
+/*
+	Higher Order Functions
+*/
+
+//Higher order functions are functions that operate on other functions, either by taking them as arguments or by returning them.
+
+//returning func()
+func sum(x, y int) int {
+	return x + y
+}
+
+func HigherOrderSum(x int) func(int) int {
+	return func(y int) int {
+		return sum(x, y)
+	}
+}
+
+//taking func() as argument
+func twice(f func(int) int) func(int) int {
+	return func(x int) int {
+		return f(f(x))
+	}
+}
+
+func HigherOrderFuncAsParameter() {
+	plusThree := func(i int) int {
+		return i + 3
+	}
+
+	g := twice(plusThree)
+
+	fmt.Println(g(7))
+}
+
+// Returning function
+func SquareSum(x int) func(int) func(int) int {
+	return func(y int) func(int) int {
+		return func(z int) int {
+			return x*x + y*y + z*z
+		}
+	}
+}
+
+/*
+	Variadic Functions
+*/
+func VariadicExample(s ...string) {
+	fmt.Println(s[0])
+	fmt.Println(s[3])
+}
+
+func AreaCalculation(str string, y ...int) int {
+	area := 1
+
+	for _, val := range y {
+		if str == "Rectangle" {
+			area *= val
+		}
+		if str == "Square" {
+			area = val * val
+		}
+	}
+
+	return area
+}
+
+func VariadicDiffTypesOfParams(i ...interface{}) {
+	for _, v := range i {
+		fmt.Println(v, " -- ", reflect.ValueOf(v).Kind())
 	}
 }
--- main.go
+++ main.go
@@ -21,4 +21,17 @@ func main() {
 	f.AnonymousFunc()
 	f.ClosuresArea()
 	f.ClosuresLoop()
+
+	partial := f.HigherOrderSum(100)
+	fmt.Println(partial(50))
+
+	f.HigherOrderFuncAsParameter()
+	squareSum := f.SquareSum(5)(6)(7)
+	fmt.Println(squareSum)
+
+	f.VariadicExample("red", "blue", "green", "yellow")
+	fmt.Println(f.AreaCalculation("Square", 20))
+	fmt.Println(f.AreaCalculation("Rectangle", 20, 30))
+	f.VariadicDiffTypesOfParams(1, "red", true, 10.5, []string{"foo", "bar", "baz"},
+		map[string]int{"apple": 23, "tomato": 13})
 }
