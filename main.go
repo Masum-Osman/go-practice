0baf55968111888a57989bccb473c450349b9392
Masum_Osman <ogkmasum@gmail.com>
2023-01-26 11:58:00 +0600

    goroutine: first try

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	f "go-practice/functions"
+	gr "go-practice/goroutines"
 	i "go-practice/interfaces"
 	pr "go-practice/panic_recover"
+	"time"
 )
 
 func main() {
@@ -45,4 +47,9 @@ func main() {
 	pr.PanicRecovery(3)
 
 	i.RunInterface()
+
+	go gr.ResponseSize("https://www.golangprograms.com")
+	go gr.ResponseSize("https://coderwall.com")
+	go gr.ResponseSize("https://stackoverflow.com")
+	time.Sleep(5 * time.Second)
 }
