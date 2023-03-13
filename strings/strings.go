37f4cb2ca319c014e7b4213bda44e33426669086
Masum_Osman <ogkmasum@gmail.com>
2023-03-13 14:02:39 +0600

    multiple tests

--- strings/strings.go
+++ strings/strings.go
@@ -1,15 +1,33 @@
 package strings
 
 import (
-	"fmt"
 	"strings"
+	"unicode"
 )
 
 func TrimString() string {
 	str := "\t Hello, World\n "
-	fmt.Printf("Before Trim Length: %d String:%v\n", len(str), str)
+	// fmt.Printf("Before Trim Length: %d String:%v\n", len(str), str)
 	trim := strings.TrimSpace(str)
-	fmt.Printf("After Trim Length: %d String:%v\n", len(trim), trim)
+	// fmt.Printf("After Trim Length: %d String:%v\n", len(trim), trim)
 
 	return trim
+}
+
+func IsOnlyLetter(s string) bool {
+	for _, r := range s {
+		if !unicode.IsLetter(r) {
+			return false
+		}
+	}
+	return true
+}
+
+func AllUpperCase(s string) bool {
+	for _, r := range s {
+		if !unicode.IsLetter(r) && !unicode.IsUpper(r) {
+			return false
+		}
+	}
+	return true
 }
--- strings/strings_test.go
+++ strings/strings_test.go
@@ -1,12 +1,53 @@
 package strings
 
-import "testing"
+import (
+	"testing"
+)
 
-func TestStringTrim(t *testing.T) {
-	got := TrimString()
-	want := "Hello, World"
+type testCases struct {
+	arg  string
+	want bool
+}
 
-	if got != want {
-		t.Errorf("Got : %s, Wanted : %s", got, want)
-	}
+func TestString(t *testing.T) {
+
+	t.Run("Is Only Letters tests: ", func(t *testing.T) {
+		cases := []testCases{
+			{"Golang", true},
+			{"Golang542", false},
+			{"Golang542", false},
+		}
+
+		for _, tc := range cases {
+			got := IsOnlyLetter(tc.arg)
+			if tc.want != got {
+				t.Errorf("Expected '%t', but got '%t'", tc.want, got)
+			}
+		}
+	})
+
+	t.Run("String Trimming Test:", func(t *testing.T) {
+		got := TrimString()
+		want := "Hello, World"
+
+		if got != want {
+			t.Errorf("Got : %s, Wanted : %s", got, want)
+		}
+	})
+
+	t.Run("Is All Upper case and only Letter test :", func(t *testing.T) {
+		cases := []testCases{
+			{"UPPERCASE", true},
+			{"LowerCase", false},
+		}
+
+		for _, tc := range cases {
+			got := AllUpperCase(tc.arg)
+
+			if tc.want != got {
+				t.Errorf("Expected '%t', but got '%t'", tc.want, got)
+			}
+		}
+	})
+
 }
