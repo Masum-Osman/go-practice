74a8d6353e4848c99ca3e53b3bf5086aaef50692
Masum_Osman <ogkmasum@gmail.com>
2023-03-20 18:28:31 +0600

    IDK! what I did

--- deque/deque.go
+++ deque/deque.go
@@ -1,5 +1,7 @@
 package deque
 
+import "fmt"
+
 type Deque struct {
 	front stack
 	back  stack
@@ -32,9 +34,90 @@ func (d *Deque) EnqueueF(x int) {
 	d.balance()
 }
 
+// enqueue back
+func (d *Deque) EnqueueB(x int) {
+	d.back.push(x)
+	d.balance()
+}
+
+// dequeue front
+func (d *Deque) DequeueF() (int, bool) {
+	var x int
+	var ok bool
+	if d.front.len() == 0 {
+		x, ok = d.back.pop()
+	} else {
+		x, ok = d.front.pop()
+	}
+	if !ok {
+		return 0, false
+	}
+	d.balance()
+	return x, true
+}
+
+// dequeue back
+func (d *Deque) DequeueB() (int, bool) {
+	var x int
+	var ok bool
+	if d.back.len() == 0 {
+		x, ok = d.front.pop()
+	} else {
+		x, ok = d.back.pop()
+	}
+	if !ok {
+		return 0, false
+	}
+	d.balance()
+	return x, true
+}
+
 func (d *Deque) balance() {
 	small, big := order(&d.front, &d.back)
 	if small.len() == 0 && big.len() == 1 {
 		return
 	}
+	if 3*d.front.len() < d.back.len() ||
+		3*d.back.len() < d.front.len() {
+		d.rebalance()
+	}
+}
+
+func (d *Deque) rebalance() {
+	small, big := order(&d.front, &d.back)
+	half := (small.len() + big.len()) / 2
+	tmpB := &stack{}
+	tmpS := &stack{}
+	mvN(tmpB, big, half) // store half of big
+	mvAll(tmpS, small)   // store small
+	mvAll(small, big)    // put bottom half of big onto small
+	mvAll(small, tmpS)   // restore small
+	mvAll(big, tmpB)     // restore big
+}
+
+func order(a, b *stack) (smallest, bigest *stack) {
+	if a.len() < b.len() {
+		return a, b
+	}
+	return b, a
+}
+
+// mvN: move n items from dst and push to src
+func mvN(dst, src *stack, n int) {
+	for i := 0; i < n; i++ {
+		x, _ := src.pop()
+		dst.push(x)
+	}
+}
+
+// mvAll: move all items from src to dst
+
+func mvAll(dst, src *stack) {
+	mvN(dst, src, src.len())
+}
+
+func (d *Deque) String() string {
+	front := fmt.Sprintf("[%v]", d.front)
+	back := fmt.Sprintf("[%v]", d.back)
+	return fmt.Sprintf("%s | %s", back, front)
 }
---
+++ deque/deque_test.go
@@ -0,0 +1,56 @@
+package deque
+
+import "testing"
+
+func TestBasic(t *testing.T) {
+	check := func(msg string, got, want int) {
+		t.Log(msg, got, want)
+		if got != want {
+			t.Errorf("%s: got: %d want: %d\n", msg, got, want)
+		}
+	}
+
+	var d Deque
+	d.EnqueueF(1)
+	d.EnqueueB(2)
+
+	if x, ok := d.DequeueF(); ok {
+		check("dequeueF", x, 1)
+	} else {
+		t.Errorf("ok error on non-empty deque\n")
+	}
+	if x, ok := d.DequeueF(); ok {
+		check("dequeueF", x, 2)
+	} else {
+		t.Errorf("ok error on non-empty deque\n")
+	}
+
+	if _, ok := d.DequeueF(); ok {
+		t.Errorf("ok error on empty deque\n")
+	}
+}
+
+func TestBalance(t *testing.T) {
+	deque := func(d *Deque, i int) {
+		x, _ := d.DequeueF()
+		if x != i {
+			t.Errorf("deque error: i: %d got: %d\n", i, x)
+		}
+	}
+
+	ntests := 10
+	var d Deque
+	for i := 0; i < ntests; i++ {
+		// no balance required
+		d.EnqueueF(i)
+		d.EnqueueB(i)
+	}
+	// force maximum re-balance
+	for i := ntests - 1; i >= 0; i-- {
+		deque(&d, i)
+	}
+	for i := 0; i < ntests; i++ {
+		deque(&d, i)
+	}
+
+}
